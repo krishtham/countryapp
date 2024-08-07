e0bda7ba7a50c3cfdf3c80bf737c17ddd86d483f
Pradeep Thamanna <tkprao@hotmail.com>
2024-08-07 19:30:06 -0400

    Spring Boot files

--- src/com/pradeep/learning/Train.java
+++
@@ -1,4 +0,0 @@
-package com.pradeep.learning;
-
-public class Train {
-}
---
+++ src/test/java/com/pradeep/Country/CountryInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.Country;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountryInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
